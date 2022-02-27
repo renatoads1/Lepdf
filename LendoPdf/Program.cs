e057e25e6571929cf99a084731be4f6e95e2a319
jeferson.imaginaction@gmail.com <jeferson.imaginaction@gmail.com>
2022-02-27 19:01:55 -0300

    first commit

---
+++ LendoPdf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace LendoPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string caminho = @"C:\PDF\Diario_3422__25_2_2022.pdf";
+            using (PdfReader reader = new PdfReader(caminho))
+            {
+                for (int pageNo = 1; pageNo <= reader.NumberOfPages; pageNo++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                    sb.Append(text);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            //agora é só tratar
+            Console.ReadLine();
+        }
+    }
+}
