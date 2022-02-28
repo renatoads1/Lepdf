c06e9edf6f286e5ff2c8c896752611ca4a72706f
jeferson.imaginaction@gmail.com <jeferson.imaginaction@gmail.com>
2022-02-27 21:31:58 -0300

    melhoria

--- LendoPdf/Program.cs
+++ LendoPdf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using iTextSharp.text.pdf;
@@ -21,10 +22,28 @@ namespace LendoPdf
                     text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
                     sb.Append(text);
                 }
+                // 1 preciso escrever este texto em arquivo com append txt
+                //Open the File
+                StreamWriter sw = new StreamWriter(@"C:\PDF\Diario_3422__25_2_2022.txt", true, Encoding.UTF8);
+                try
+                {
+                    //Write out the numbers 1 to 10 on the same line.
+                    sw.Write(sb);
+                    //close the file
+                    sw.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                    Console.ReadLine();
+                }
+                finally
+                {
+                    sw.Close();
+                }
             }
-            Console.WriteLine(sb.ToString());
-            //agora é só tratar
-            Console.ReadLine();
+            // 2 pegar linha por linha do txt separando em blocos
+            
         }
     }
 }
